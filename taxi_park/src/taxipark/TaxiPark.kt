ba1ec23755117ff4eada3a629fc35d41b479a071
bjuanyshev <bjuanyshev@greet-go.com>
2020-09-21 12:13:52 +0600

    Закончил курс

--- taxi_park/src/taxipark/TaxiPark.kt
+++ taxi_park/src/taxipark/TaxiPark.kt
@@ -12,13 +12,9 @@ data class Passenger(val name: String)
 data class Trip(
   val driver: Driver,
   val passengers: Set<Passenger>,
-  // the trip duration in minutes
   val duration: Int,
-  // the trip distance in km
   val distance: Double,
-  // the percentage of discount (in 0.0..1.0 if not null)
   val discount: Double? = null
 ) {
-  // the total cost of the trip
   val cost: Double get() = (1 - (discount ?: 0.0)) * (duration + distance)
 }
